d3619a4d89ac3871b09a025d3bd6f9a6cd12936c
Mo <Mhd.shn.web.edu@gmail.com>
2024-12-21 18:36:04 +0530

    built

--- frontend/src/Backendurl.js
+++ frontend/src/Backendurl.js
@@ -1,5 +1,5 @@
-//export const backEndURL = "http://3.83.46.122:3001"
-export const backEndURL = "http://localhost:3001";
+export const backEndURL = "http://54.167.125.92:3001"
+//export const backEndURL = "http://localhost:3001";
 
-//export const imageURL = "http://3.83.46.122:3001"
-export const imageURL = "http://localhost:3001"
+export const imageURL = "http://54.167.125.92:3001"
+//export const imageURL = "http://localhost:3001"
