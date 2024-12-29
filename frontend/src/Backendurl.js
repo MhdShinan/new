1a9630f7c2351b8a96b52a96011ccb434e5a4daf
Mo <Mhd.shn.web.edu@gmail.com>
2024-12-29 21:44:04 +0530

    build

--- frontend/src/Backendurl.js
+++ frontend/src/Backendurl.js
@@ -1,5 +1,5 @@
-//export const backEndURL = "http://3.88.140.65:3001";
-export const backEndURL = "http://localhost:3001";
+export const backEndURL = "http://107.23.45.189:3001";
+//export const backEndURL = "http://localhost:3001";
 
-//export const imageURL = "http://3.88.140.65:3001";
-export const imageURL = "http://localhost:3001";
+export const imageURL = "http://107.23.45.189:3001";
+//export const imageURL = "http://localhost:3001";
