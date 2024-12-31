68696bbd240bf295cd76f042a1dc02f56a063bd2
Mo <Mhd.shn.web.edu@gmail.com>
2024-12-31 20:50:50 +0530

    Update Backendurl.js

--- frontend/src/Backendurl.js
+++ frontend/src/Backendurl.js
@@ -1,6 +1,8 @@
-export const backEndURL = "https://54.164.109.11:3001";
+// export const backEndURL = "https://54.164.109.11:3001";
+export const backEndURL = "https://api.rtechsl.com/api";
 //export const backEndURL = "http://localhost:3001";
 
-export const imageURL = "https://54.164.109.11:3001";
+// export const imageURL = "https://54.164.109.11:3001";
+export const imageURL = "https://api.rtechsl.com/api";
 //export const imageURL = "http://localhost:3001";
 
