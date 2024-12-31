ebd898f82a4cedabd8c41d03992aeb6e192ba444
Mo <Mhd.shn.web.edu@gmail.com>
2024-12-31 22:15:57 +0530

    build

--- frontend/src/Backendurl.js
+++ frontend/src/Backendurl.js
@@ -1,10 +1,11 @@
 // export const backEndURL = "https://54.164.109.11:3001";
-export const backEndURL = "http://54.164.109.11:3001";
+//export const backEndURL = "http://54.164.109.11:3001";
 //export const backEndURL = "http://localhost:3001";
 
 // export const imageURL = "https://54.164.109.11:3001";
-export const imageURL = "http://54.164.109.11:3001";
+//export const imageURL = "https://54.164.109.11:3001";
 //export const imageURL = "http://localhost:3001";
 
-// export const backEndURL = "https://rtechsl.com/api";
-// export const imageURL = "https://rtechsl.com/api";
+export const backEndURL = "https://rtechsl.com/api";  // Use HTTPS instead of HTTP
+export const imageURL = "https://rtechsl.com/api";    // Ensure image URL also uses HTTPS
+
