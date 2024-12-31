74a5fd6ede7f4bb7c3e6b02c1bd60206b4b7cd8a
Mo <Mhd.shn.web.edu@gmail.com>
2024-12-31 20:14:10 +0530

    done

--- frontend/src/Backendurl.js
+++ frontend/src/Backendurl.js
@@ -1,12 +1,6 @@
-export const backEndURL = "https://54.204.63.231:3001";
+export const backEndURL = "https://100.26.201.222:3001";
 //export const backEndURL = "http://localhost:3001";
 
-export const imageURL = "https://54.204.63.231:3001";
+export const imageURL = "https://100.26.201.222:3001";
 //export const imageURL = "http://localhost:3001";
 
-
-// const backEndURL = 'http://localhost:3001'; // Old
-// const backEndURL = 'https://rtechsl.com/api'; // Updated
-
-// const imageURL = 'http://localhost:3001/static'; // Old
-// const imageURL = 'https://rtechsl.com/static'; // Updated
