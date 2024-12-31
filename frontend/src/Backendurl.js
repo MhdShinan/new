0889e55687c50dabe995093992b422a7fedee8d1
Mo <Mhd.shn.web.edu@gmail.com>
2024-12-31 20:21:52 +0530

    delete

--- frontend/src/Backendurl.js
+++ frontend/src/Backendurl.js
@@ -1,6 +1,6 @@
-export const backEndURL = "https://100.26.201.222:3001";
+export const backEndURL = "https://54.164.109.11:3001";
 //export const backEndURL = "http://localhost:3001";
 
-export const imageURL = "https://100.26.201.222:3001";
+export const imageURL = "https://54.164.109.11:3001";
 //export const imageURL = "http://localhost:3001";
 
